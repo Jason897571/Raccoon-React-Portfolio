2f22f6c636d7495e2bcf07594177331bbb9daa89
Jason897571 <king.gs1314@outlook.com>
2024-04-10 17:32:09 -0400

    complet navigation component

--- RaccoonReactPortfolio/src/components/Navigation.jsx
+++ RaccoonReactPortfolio/src/components/Navigation.jsx
@@ -1,25 +1,30 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const NavTab = ()=>{
+
+    const currentPage = useLocation().pathname;
+
     return(
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">Jason Gu</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <Link className={currentPage === "/About" ? "nav-link active" : "nav-link"} aria-current="page" to="/About">About Me</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Features</a>
+                        <Link className={currentPage === "/Portfolio" ? "nav-link active" : "nav-link"} aria-current="page" to="/Portfolio">Portfolio</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
+                        <Link className={currentPage === "/Contact" ? "nav-link active" : "nav-link"} aria-current="page" to="/Contact">Contact</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
+                        <Link className={currentPage === "/Resume" ? "nav-link active" : "nav-link"} aria-current="page" to="/Resume">Resume</Link>
                     </li>
                 </ul>
                 </div>
