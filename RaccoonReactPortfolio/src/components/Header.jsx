2e5cb1993d75bf4a28daf7fbd7ae302e07c2dae4
Jason897571 <king.gs1314@outlook.com>
2024-04-10 17:01:43 -0400

    create sudo code for each page

---
+++ RaccoonReactPortfolio/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import NavTab from './Navigation'
+
+const Header = () => {
+    return (
+        <header>
+            <NavTab />
+        </header>
+    )
+}
+
+
+export default Header;
--- RaccoonReactPortfolio/src/main.jsx
+++ RaccoonReactPortfolio/src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App.jsx'
+import AboutPage from './pages/About.jsx'
+import PortfolioPage from './pages/Portfolio.jsx'
+import ContactPage from './pages/Contact.jsx'
+import ErrorPage from './pages/Error.jsx'
+import ResumePage from './pages/Resume.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: '/About',
+        element: <AboutPage />,
+      },
+      {
+        path: '/Portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: '/Contact',
+        element: <ContactPage />,
+      },
+      {
+        path: '/Resume',
+        element: <ResumePage />,
+      }
+    ]
+    
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
